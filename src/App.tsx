ff99df71efd00df705cdfc078f7ec4eb45fafdfa
Pedro Henrique <karpatech2025@gmail.com>
2025-08-29 13:06:05 -0300

    Pt. 1 BlogPessoal React

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,37 @@
-import './App.css'
-import Home from "./pages/home/Home";
+// import Footer from "./components/footer/Footer"
+// import Navbar from "./components/navbar/Navbar"
+// import Home from "./pages/home/Home"
+// import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+// function App() {
+//   return (
+//       <BrowserRouter>
+//           <Navbar />
+//           <div className="min-h-[80vh]">
+//             <Routes>
+//               <Route path="/" element={<Home />} />
+//               <Route path="/home" element={<Home />} />
+//             </Routes>
+//           </div>
+//           <Footer />
+//         </BrowserRouter>
+//   )
+// }
+// export default App
+
+
+import Footer from './components/footer/Footer';
+import Navbar from './components/navbar/Navbar';
+import Home from './pages/home/Home';
 
 function App() {
   return (
     <>
-    <Home/>
+      <Navbar />
+      <Home />
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
