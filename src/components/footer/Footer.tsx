9c9ed96efbd0ae91bba96775d4fee24478ad28af
Pedro Henrique <karpatech2025@gmail.com>
2025-08-29 13:38:02 -0300

    Pt. 2 BlogPessoal React

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -12,11 +12,17 @@ function Footer() {
                             Blog Pessoal Generation | Copyright: {data}
                         </p>
                     <p className='text-lg'>Acesse nossas redes sociais</p>
-                    <div className='flex gap-2'>
-                        <LinkedinLogoIcon size={48} weight='bold' />
-                        <InstagramLogoIcon size={48} weight='bold' />
-                        <FacebookLogoIcon size={48} weight='bold' />
-                    </div>
+                  <div className='flex gap-2'>
+	<a href="https://www.linkedin.com/in/pedro-henrique-o13/" target="_blank">
+    	<LinkedinLogoIcon size={48} weight='bold' />
+    </a>
+    <a href="https://www.instagram.com/generationbrasil/" target="_blank">
+    	<InstagramLogoIcon size={48} weight='bold' />
+    </a>
+    <a href="https://www.facebook.com/generationbrasil/" target="_blank">
+    	<FacebookLogoIcon size={48} weight='bold' />
+	</a>
+</div>
                 </div>
             </div>
         </>
