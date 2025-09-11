f3ebd5c58739b6424f6edd214ed1c60b6b523057
Pedro Henrique <karpatech2025@gmail.com>
2025-09-11 08:51:13 -0300

    Atualização

--- src/pages/cadastro/Cadastro.tsx
+++ src/pages/cadastro/Cadastro.tsx
@@ -24,7 +24,7 @@ function Cadastro() {
   }, [usuario]);
 
   function retornar() {
-    navigate("/login");
+    navigate("/");
   }
 
   function atualizarEstado(e: ChangeEvent<HTMLInputElement>) {
